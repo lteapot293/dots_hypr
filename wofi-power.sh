e3c998e617f8dbd0235c990a6e989e6dd68a01a1
lteapot293 <123525317+lteapot293@users.noreply.github.com>
2023-11-22 20:26:24 +0300

    Add files via upload

---
+++ wofi-power.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+entries="⇠ Logout\n󰑙 Reboot\n⏻ Shutdown"
+
+selected=$(echo -e $entries|wofi --width 250 --height 210 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
+
+case $selected in
+  logout)
+     hyprctl dispatch exit x;;
+  reboot)
+    exec systemctl reboot;;
+  shutdown)
+    exec systemctl poweroff -i;;
+esac
